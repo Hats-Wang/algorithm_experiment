567711510df9df4c088e1abb32222e3f27b99e82
Hats-Wang <2741403628@qq.com>
2020-10-25 13:46:57 +0800

    add detailed comment

--- exp2.cpp
+++ exp2.cpp
@@ -6,6 +6,7 @@
 int main (){
 	int total_length, num, left, right, mid;
 	std::cin>> total_length >> num;
+        // the absolute distance of each point from 0 to last.
 	int  abs_distance[num + 2];
 
 	//initialize the absolute distance of each point.
@@ -14,9 +15,11 @@ int main (){
 	abs_distance[num + 1] = total_length;
 	std::sort(abs_distance, abs_distance + num + 1);
 	
+	//min distance from point i to point j.
 	int ans[num + 2][num + 2];
 	std::memset(ans, 0, (num + 2)*(num + 2)*sizeof(int));
 	
+	//min_cost[i][j] = min(min_cost[i][mid] + min_cost[mid][j]) + leng_of_(i,j).
 	for(left = num - 1; left >= 0; left--){
 	    for (right = left + 2; right <= num + 1; right++){
 	    	ans[left][right] = INT_MAX;
